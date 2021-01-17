ec01cf2cc0c355c159f8be89dcc5b1ea763ec1f3
LyubovTatur <lubatatur58@gmail.com>
2021-01-17 17:27:13 +0300

    done

---
+++ kpyap_laba_15_2/kpyap_laba_15_2/Game.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace kpyap_laba_15_2
+{
+    [Serializable]
+    public class Game
+    {
+        public delegate void Handler(string message);
+        public event Handler Notify;
+
+
+        int points;
+        int visitorsPerDay;
+        int remainingMoves;
+        int costumes;
+        int pointsToWin;
+
+        const int robberyProbability = 20;
+        const int CostumeCreationPrice = 20;
+        const int CostumeSellingPrice = 50;
+
+
+
+        public Game(int movesCount, int visitorsPerDay, int startPoints, int pointsToWin)
+        {
+            this.points = startPoints;
+            this.visitorsPerDay = visitorsPerDay;
+            this.remainingMoves = movesCount;
+            this.pointsToWin = pointsToWin;
+        }
+        public void Ad()
+        {
+            points -= 100;
+            visitorsPerDay *= 2;
+        }
+        public void TakeMeasurements()
+        {
+            points -= 15;
+            visitorsPerDay += visitorsPerDay / 4;
+        }
+
+        public void SewCostumes(int amount)
+        {
+            points -= CostumeCreationPrice * amount;
+            costumes += amount;
+        }
+
+        public void Robbery()
+        {
+            if (costumes!=0)
+            {
+                Notify?.Invoke("Ой.. Ваши костюмы были украдены...");
+                costumes = 0;
+            }
+            else
+            {
+                Notify?.Invoke("Банда бандитов пришла воровать ваши костюмы, но их...... и так не было...");
+            }
+        }
+
+        public string CurrentInfo()
+        {
+            return $"____________\n\nходов осталось - {remainingMoves}\nочки - {points}\nпосетителей за ход - {visitorsPerDay}\nкостюмов имеется - {costumes}\n____________\n ";
+        }
+
+        public void SaveGame()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream("SavedGame.txt", FileMode.OpenOrCreate))
+            {
+                bf.Serialize(fs, this);
+            }
+        }
+
+        public void BuyingСostumes(int amount)
+        {
+            if (amount > costumes)
+            {
+                amount = costumes;
+            }
+            costumes -= amount;
+            points += amount * CostumeSellingPrice;
+            Notify?.Invoke($"Сегодня было продано {amount} костюмов (+{amount * CostumeSellingPrice} очков).") ;
+            
+        }
+
+        public bool GameStep()
+        {
+            bool IsContinue = true;
+            remainingMoves--;
+            if (remainingMoves == 0 || points < 0)
+            {
+                Notify?.Invoke("Вы проиграли.");
+                IsContinue = false;
+            }
+            Random random = new Random();
+            int boughtСostumes = random.Next(visitorsPerDay);
+            if (boughtСostumes > 0 && costumes > 0)
+            {
+                BuyingСostumes(boughtСostumes);
+            }
+            if (random.Next(100) < robberyProbability)
+            {
+                Robbery();
+            }
+            if (points >= pointsToWin)
+            {
+                Notify?.Invoke("Вы выйграли!!!!");
+                IsContinue = false;
+            }
+            return IsContinue;
+        }
+
+    }
+}
+// Игра Ателье. Задача получить n очков, имея m, за t ходов. Если очков
+// меньше 0 игрок проигрывает. Количество посетителей в день p.
+// - провести рекламу. Стоит 100 очков х2 посетителей.
+// - снять мерки. Стоит 15 очков +0.25х посетителей.
+// - Сшить костюм. Указывается количество. Костюмы покупаются не в
+// большем количестве, чем приходят посетители в ход. Стоит 20 очков. При
+// покупке 50 очков.
+// Случайно может прийти банда в начале хода и украсть все костюмы.
+// Вероятность этого 20%. Генерироваться событие, когда это происходит
---
+++ kpyap_laba_15_2/kpyap_laba_15_2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace kpyap_laba_15_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("1. Начать новую игру \n2. Загрузить игру");
+                string choice;
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Введите начальное количество очков.");
+                            int points = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите количество ходов.");
+                            int steps = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите количество очков, которое нужно набрать для выйгрыша.");
+                            int winPoints = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите количество посетителей за ход.");
+                            int visitors = int.Parse(Console.ReadLine());
+                            Game game = new Game(steps, visitors, points, winPoints);
+                            game.Notify += DisplayMessage;
+                            Gameplay(game);
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            Game game = LoadGame();
+                            game.Notify += DisplayMessage;
+                            Gameplay(game);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        static Game LoadGame()
+        {
+            // сериализя
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream("SavedGame.txt", FileMode.Open))
+            {
+               return (Game)bf.Deserialize(fs);
+            }
+        }
+
+        static void Gameplay(Game game)
+        {
+            while (game.GameStep())
+            {
+                bool Step = true;
+                while (Step)
+                {
+                    Console.WriteLine(game.CurrentInfo());
+                    Console.WriteLine("1. Реклама (-100 очков, удвоение посетителей)");
+                    Console.WriteLine("2. Снять мерки (-15 очков, посетителей х 1.25)");
+                    Console.WriteLine("3. Сшить костюмы (-20 очков\\костюм за пошив, +50 очков\\костюм за продажу)");
+                    Console.WriteLine("4. Завершить ход.");
+                    string choice;
+                    choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            game.Ad();
+                            break;
+                        case "2":
+                            game.TakeMeasurements();
+                            break;
+                        case "3":
+                            Console.WriteLine("Введите количество костюмов.");
+                            int amount = int.Parse(Console.ReadLine());
+                            game.SewCostumes(amount);
+                            break;
+                        case "4":
+                            Step = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            }
+        }
+
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+    }
+}
+// Игра Ателье. Задача получить n очков, имея m, за t ходов. Если очков
+// меньше 0 игрок проигрывает. Количество посетителей в день p.
+// - провести рекламу. Стоит 100 очков х2 посетителей.
+// - снять мерки. Стоит 15 очков +0.25х посетителей.
+// - Сшить костюм. Указывается количество. Костюмы покупаются не в
+// большем количестве, чем приходят посетители в ход. Стоит 20 очков. При
+// покупке 50 очков.
+// Случайно может прийти банда в начале хода и украсть все костюмы.
+// Вероятность этого 20%. Генерироваться событие, когда это происходит
